ae948651c75ffcdffe2e7ae62675039679d750ba
shahin <sarker4292@gmail.com>
2025-12-15 11:58:04 +0600

    added app constant

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,4 @@
+class AppConstants{
+  static const String apiBaseUrl = "https://api.github.com/";
+  static const String searchUrl = "search/repositories?q=Flutter&sort=stars&order=desc&per_page=50";
+}
